6d26745971db489068feb52c6080bb4911381ab1
Fluke <lingfluke@gmail.com>
2021-09-20 18:52:24 +0700

    update ui account boost

--- sampleUI/Components/UIViews/UICustomView.swift
+++ sampleUI/Components/UIViews/UICustomView.swift
@@ -16,4 +16,25 @@ import UIKit
         }
     }
     
+    @IBInspectable var shadowRadius: CGFloat = CGFloat(0.5) {
+        didSet{
+            layer.masksToBounds = false
+            layer.shadowRadius = shadowRadius
+        }
+    }
+
+    @IBInspectable var shadowOpacity: Float = 0.4 {
+        didSet{
+            layer.masksToBounds = false
+            layer.shadowOpacity = shadowOpacity
+        }
+    }
+
+    @IBInspectable var shadowOffset: CGSize = CGSize(width: 1, height: 4) {
+        didSet{
+            layer.masksToBounds = false
+            layer.shadowOffset = shadowOffset
+        }
+    }
+    
 }
